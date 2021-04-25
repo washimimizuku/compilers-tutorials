780f340b2fb160d346c7c5a1e474e76335a108f6
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-04-25 09:59:43 +0100

    Create parser for simple calculator

---
+++ simple_calculator/parser.py
@@ -0,0 +1,63 @@
+from sly import Parser
+
+class BasicParser(Parser):
+    # Tokens are passed from lexer to parser
+    tokens = BasicLexer.tokens
+
+    precedence = (
+        ('left', '+', '-'),
+        ('left', '*', '/'),
+        ('right', 'UMINUS'),
+    )
+
+    def __init__(self):
+        self.env = {}
+
+    @_('')
+    def statement(self, p):
+        # Empty on purpose
+        pass
+
+    @_('var_assign')
+    def statement(self, p):
+        return p.var_assign
+
+    @_('NAME "=" expr')
+    def var_assign(self, p):
+        return ('var_assign', p.NAME, p.expr)
+
+    @_('NAME "=" STRING')
+    def var_assign(self, p):
+        return ('var_assign', p.NAME, p.STRING)
+
+    @_('expr')
+    def statement(self, p):
+        return (p.expr)
+
+    @_('expr "+" expr')
+    def expr(self, p):
+        return ('add', p.expr0, p.expr1)
+
+    @_('expr "-" expr')
+    def expr(self, p):
+        return ('sub', p.expr0, p.expr1)
+
+    @_('expr "*" expr')
+    def expr(self, p):
+        return ('mul', p.expr0, p.expr1)
+
+    @_('expr "/" expr')
+    def expr(self, p):
+        return ('div', p.expr0, p.expr1)
+
+    @_('"-" expr %prec UMINUS')
+    def expr(self, p):
+        return p.expr
+    
+    @_('NAME')
+    def expr(self, p):
+        return ('var', p.NAME)
+    
+    @_('NUMBER')
+    def expr(self, p):
+        return ('num', p.NUMBER)
