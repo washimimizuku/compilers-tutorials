aad5e4c3964155d3e3aa191f80a4ba61fdf9c4d8
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-04-29 13:29:44 +0100

    Add parser base class

---
+++ monkey-language/parser.py
@@ -0,0 +1,24 @@
+from ast import Program
+from lexer import Lexer
+from token_ import Token
+
+
+class Parser():
+    def __init__(self, lexer: Lexer) -> None:
+        self.lexer = lexer
+        self.current_token = None
+        self.peek_token = None
+
+        # Read two tokens, so current_token and peek_token are both set
+        self.next_token()
+        self.next_token()
+
+
+    def next_token(self) -> None:
+        self.current_token = self.peek_token
+        self.peek_token = self.lexer.next_token()
+
+
+    def parse_program(self) -> Program:
+        return None
+    
