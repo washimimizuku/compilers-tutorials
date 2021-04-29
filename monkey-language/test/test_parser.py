b84ea6ad028f49ac145189285a7a434558ad8ff1
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-04-29 16:36:08 +0100

    Add tests for parser

--- monkey-language/test/test_parser.py
+++ monkey-language/test/test_parser.py
@@ -1,3 +1,4 @@
+from monkey.ast import Statement, LetStatement
 from monkey.lexer import Lexer
 from monkey.parser import Parser
 import unittest
@@ -16,9 +17,19 @@ let foobar = 838383;
 
         program = parser.parse_program()
 
-        self.assertNotEqual(program, None)
-        self.assertEqual(len(program.statements), 3)
-        
+        self.assertNotEqual(program, None, "parse_program() returned None")
+        self.assertEqual(len(program.statements), 3, f"program.Statements does not contain 3 statements. got={len(program.statements)}")
+
+        expected = ['x', 'y', 'foobar']
+        for i in range(3):
+            self._test_let_statement(program.statements[i], expected[i])
+
+    def _test_let_statement(self, statement: Statement, name: str):
+        self.assertEqual(statement.token_literal(), 'let', f"statement.token_literal not 'let'. got={statement.token_literal()}")
+        self.assertIsInstance(statement, LetStatement, f"statement not {name}. got={statement}")
+        self.assertEqual(statement.name.value, name, f"statement.name.value not {name}. got={statement.name.value}")
+        self.assertEqual(statement.name.token_literal(), name, f"statement not {name}. got={statement.name.token_literal()}")
+
 
 if __name__ == '__main__':
     unittest.main()
