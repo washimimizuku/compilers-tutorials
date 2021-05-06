92230a34c83002fbf30fe4c7fac0f034e52ae953
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-06 20:19:02 +0200

    Update repl to use new parser

--- monkey-language/repl.py
+++ monkey-language/repl.py
@@ -1,18 +1,43 @@
 from monkey.lexer import Lexer
+from monkey.parser import Parser
 from monkey.token import Token, TokenType
 
 PROMPT = '>> '
+MONKEY_FACE = '''
+            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \\
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"`` ``"-./, -' /
+   `'-' /_   ^ ^   _\ '-'`
+       |  \._   _./  |
+       \   \ `~` /   /
+        '._ '-=-' _.'
+           '~---~'
+'''
 
 
 def start() -> None:
     while True:
         line = input(PROMPT)
-        lex = Lexer(line)
 
-        tok = Token('', '')
-        while tok.token_type != TokenType.EOF:
-            tok = lex.next_token()
-            print(tok)
+        lex = Lexer(line)
+        pars = Parser(lex)
+        program = pars.parse_program()
+
+        if len(pars.errors) != 0:
+            print_parser_errors(pars.errors)
+        else:
+            print(f"{str(program)}")
+
+
+def print_parser_errors(errors: str):
+    for error in errors:
+        print(MONKEY_FACE)
+        print("Woops! We ran into some monkey business here!")
+        print(" parser errors:")
+        print(f"\t{error}")
 
 
 if __name__ == "__main__":
